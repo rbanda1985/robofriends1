eef8e2819f8dce1c878cb2de28ef54b7735f6d76
rbanda1985 <robertbanda85@gmail.com>
2023-02-04 05:00:17 -0600

    Completed Project

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component }from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css';
+import Scroll from '../components/Scroll';
+
+
+class App extends Component  {
+  constructor(){
+    super()
+    this.state = {
+      searchField: '',
+      robots: []
+    }
+  }
+
+  async componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {this.setState({robots: users})})
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+
+  render(){
+    const { robots, searchField } = this.state;
+    const filteredSearch = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredSearch}/>
+        </Scroll>
+      </div>
+    )
+  }
+}
+
+export default App
